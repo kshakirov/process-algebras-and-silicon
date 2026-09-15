f1c951d8d085fa191ab1bdb6e971106d73285437
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-09-15 22:40:36 +0300

    experiment 2

--- src/elixir/beam_fsm/lib/beam_fsm/scheduler.ex
+++ src/elixir/beam_fsm/lib/beam_fsm/scheduler.ex
@@ -13,20 +13,22 @@ defmodule BeamFsm.Scheduler do
   def update_status(pid, counts), do: GenServer.call(pid, {:update_status, counts})
   def increment_status(pid), do: GenServer.call(pid, :increment_status)
 
+
+  
   @impl true
   def handle_call({:update_status, counts}, _from, state) do
-    new_state = state + counts
+    new_state = %{state |  counter:  state.counter + counts}
     {:reply, new_state, new_state}
   end
 
   @impl true
   def handle_call(:increment_status, _from, state) do
-    new_state = state + 1
+    new_state = %{state |  counter:  state.counter + 1}
     {:reply, new_state, new_state}
   end
 
   @impl true
   def handle_call(:get_status, _from, state) do
-    {:reply, state, state}
+    {:reply, state.counter, state.counter}
   end
 end
--- src/elixir/beam_fsm/test/beam_fsm_test.exs
+++ src/elixir/beam_fsm/test/beam_fsm_test.exs
@@ -4,20 +4,20 @@ defmodule BeamFsm.SchedulerTest do
   use ExUnit.Case, async: true
 
   test "стартует с начальным значением" do
-    pid = start_supervised!({BeamFsm.Scheduler, 10})
-    assert BeamFsm.Scheduler.get_status(pid) == 10
+    pid = start_supervised!({BeamFsm.Scheduler, %{counter: 0, fact: 0, predicate: true}})
+    assert BeamFsm.Scheduler.get_status(pid) ==0
   end
 
   test "increment_status" do
-    pid = start_supervised!({BeamFsm.Scheduler, 0})
-    assert BeamFsm.Scheduler.increment_status(pid) == 1
-    assert BeamFsm.Scheduler.increment_status(pid) == 2
+    pid = start_supervised!({BeamFsm.Scheduler, %{counter: 0, fact: 0, predicate: true}})
+    BeamFsm.Scheduler.increment_status(pid)
+    BeamFsm.Scheduler.increment_status(pid)
     assert BeamFsm.Scheduler.get_status(pid) == 2
-  end
+ end
 
   test "update_status" do
-    pid = start_supervised!({BeamFsm.Scheduler, 5})
-    assert BeamFsm.Scheduler.update_status(pid, 7) == 12
+    pid = start_supervised!({BeamFsm.Scheduler, %{counter: 5, fact: 0, predicate: true}})
+    BeamFsm.Scheduler.update_status(pid, 7) 
     assert BeamFsm.Scheduler.get_status(pid) == 12
   end
 end
