5991c24951b55d864696c7afb36f7a8f911c7cb3
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-09-22 22:54:14 +0300

    experiment 8

--- src/elixir/beam_fsm/lib/beam_fsm/scheduler.ex
+++ src/elixir/beam_fsm/lib/beam_fsm/scheduler.ex
@@ -11,37 +11,13 @@ defmodule BeamFsm.Scheduler do
   @impl true
   def init(initial), do: {:ok, initial}
 
-  def get_status(pid), do: GenServer.call(pid, :get_status)
-  def update_status(pid, counts), do: GenServer.call(pid, {:update_status, counts})
-  def increment_status(pid), do: GenServer.call(pid, :increment_status)
-
   def submit_task(pid, task), do: GenServer.call(pid, {:submit_task, task})  
 
   def add_fact(pid, fact), do: GenServer.call(pid, {:add_fact, fact})  
   
 
   
-  @impl true
-  def handle_call({:update_status, counts}, _from, state) do
-    new_state = %{state |  counter:  state.counter + counts}
-    {:reply, new_state, new_state}
-  end
-
-  @impl true
-  def handle_call(:increment_status, _from, state) do
-    new_state = %{state |  counter:  state.counter + 1}
-    case state.counter do
-      n when n < 5 ->      {:reply, new_state, new_state}
-      5            ->     {:reply, 1000, %{state |  counter:  1000}}
-    end
-
-  end
-
-  @impl true
-  def handle_call(:get_status, _from, state) do
-    {:reply, state.counter, state.counter}
-  end
-
+  
   @impl true
   def handle_call({:submit_task, task}, _from, state) do
     new_state = %{state |   tasks: [ task | state.tasks] }
